c119e5f7bfb76d1dfc9755ba169c173182cdfd91
Phil Howard <github@gadgetoid.com>
2025-02-26 14:46:38 +0000

    CI: Make ruff happy.

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,14 +1,9 @@
-import mock
-
-
 def test_moisture_setup(gpiod, gpiodevice, smbus2):
-    from datetime import timedelta
-
     from grow.moisture import Moisture
 
-    ch1 = Moisture(channel=1)
-    ch2 = Moisture(channel=2)
-    ch3 = Moisture(channel=3)
+    _ = Moisture(channel=1)
+    _ = Moisture(channel=2)
+    _ = Moisture(channel=3)
 
 
 def test_moisture_read(gpiod, gpiodevice, smbus2):
@@ -24,12 +19,12 @@ def test_moisture_read(gpiod, gpiodevice, smbus2):
 
 
 def test_pump_setup(gpiod, gpiodevice, smbus2):
-    from grow.pump import PUMP_PWM_FREQ, Pump
+    from grow.pump import Pump
     from grow.pwm import PWM
 
-    ch1 = Pump(channel=1)
-    ch2 = Pump(channel=2)
-    ch3 = Pump(channel=3)
+    _ = Pump(channel=1)
+    _ = Pump(channel=2)
+    _ = Pump(channel=3)
 
     # Threads. Not even once.
     PWM.stop_thread()
